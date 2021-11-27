401f06a544586973f458e095019fcdef2655f536
Ryo Asai <fu.orangina0516@iCloud.com>
2021-11-27 11:51:39 +0900

    feature:イベント更新用API作成

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/events', to: 'events#index'
   get '/events/:id', to: 'events#show'
-  post 'events', to: 'events#create'
+  post '/events', to: 'events#create'
+  put '/events/:id', to: 'events#update'
 end

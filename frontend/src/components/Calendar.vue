0cdf301185976c9b5e5591cbcd3cdf5f244270f5
Ryo Asai <fu.orangina0516@iCloud.com>
2021-12-03 22:52:33 +0900

    axiosインストール

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -3,7 +3,7 @@
     <h1>Calendar</h1>
     <p>events:</p>
     <p>{{ events }}</p>
-    <button type="submit">fetchEvents</button>
+    <button type="submit" @click="fetchEvents()">fetchEvents</button>
   </div>
 </template>
 
@@ -11,7 +11,20 @@
 export default {
   name: 'Calendar',
   data: () => ({
-    events: []
+    events: [],
   }),
+  methods: {
+    fetchEvents() {
+      // GETリクエストを送信し、取得データをevents変数に代入する
+      axios
+        .get('http://localhost:3000/events')
+        .then((response) => {
+          this.events = response.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
 };
 </script>

e4a0c940322c7f4cd0c23ff3e7d54e9aa32f1476
Ryo Asai <fu.orangina0516@iCloud.com>
2021-12-11 00:03:53 +0900

    fix:終了日の日付選択フォーム作成

--- frontend/src/components/EventFormDialog.vue
+++ frontend/src/components/EventFormDialog.vue
@@ -11,6 +11,7 @@
       </DialogSection>
       <DialogSection icon="mdi-clock-outline">
         <DateForm v-model="startDate" />
+        <DateForm v-model="endDate" />
       </DialogSection>
     </v-card-text>
     <v-card-actions class="d-flex justify-end"> <v-btn @click="submit">保存</v-btn> </v-card-actions>Ï
@@ -32,12 +33,14 @@ export default {
   data: () => ({
     name: '',
     startDate: null,
+    endDate: null,
   }),
   computed: {
     ...mapGetters('events', ['event']),
   },
   created() {
     this.startDate = format(this.event.start, 'yyyy/MM/dd');
+    this.endDate = format(this.event.end, 'yyyy/MM/dd');
   },
   methods: {
     ...mapActions('events', ['setEvent', 'setEditMode', 'createEvent']),
@@ -49,7 +52,7 @@ export default {
       const params = {
         name: this.name,
         start: this.startDate,
-        end: this.event.end,
+        end: this.endDate,
       };
       this.createEvent(params);
       this.closeDialog();
